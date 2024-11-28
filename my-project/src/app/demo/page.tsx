490048906959aa692e37eb425149463a0db397e8
Dxeg0o <dsoler.olguin@gmail.com>
2024-11-27 21:41:16 -0300

    link

--- my-project/src/app/demo/page.tsx
+++ my-project/src/app/demo/page.tsx
@@ -27,7 +27,7 @@ export default function YouTubePlayer() {
                 <div className="relative w-full h-0 pb-[56.25%]">
                   <iframe
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/ykexYS9u_hc?si=2p5AdrxPeCuV3uWT"
+                    src="https://www.youtube.com/embed/hGwvJkDRiz0?si=MGJaLJ5uRsyIziio"
                     title="STASS Demo"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
