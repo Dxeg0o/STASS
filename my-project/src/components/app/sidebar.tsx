dea7275e1978261f447c385caaf5f09c97303bea
Dxeg0o <dsoler.olguin@gmail.com>
2024-12-09 20:48:58 -0300

    update

---
+++ my-project/src/components/app/sidebar.tsx
@@ -0,0 +1,62 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { BarChart2, Tag } from "lucide-react";
+// Menu items.
+const items = [
+  {
+    title: "Inicio",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Análisis",
+    url: "#",
+    icon: BarChart2,
+  },
+  {
+    title: "Etiquetas",
+    url: "#",
+    icon: Tag,
+  },
+  {
+    title: "Configuraciones",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menú</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
