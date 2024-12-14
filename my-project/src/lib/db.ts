38c73f629618ed48574c3db73718b4d35534fb5d
Cseguuu <cristobalsegu@gmail.com>
2024-12-14 18:06:38 -0300

    backend auth

---
+++ my-project/src/lib/db.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+export const connectDB = async () => {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(MONGO_URI);
+    console.log('Connected to MongoDB');
+  }
+};
