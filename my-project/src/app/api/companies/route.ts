7810545104e9e916ab515f8db11be53ef97498d3
Dxeg0o <dsoler.olguin@gmail.com>
2024-12-16 01:57:16 -0300

    add try catch to companies

--- my-project/src/app/api/companies/route.ts
+++ my-project/src/app/api/companies/route.ts
@@ -3,22 +3,33 @@ import { NextResponse, NextRequest } from "next/server";
 import { connectDb } from "@/lib/mongodb";
 import Empresa from "@/models/companies";
 import mongoose from "mongoose";
+
 export async function GET() {
-  await connectDb();
+  try {
+    await connectDb();
 
-  const companies = await Empresa.find();
+    const companies = await Empresa.find();
 
-  return NextResponse.json(companies);
+    return NextResponse.json(companies);
+  } catch (error) {
+    console.error("Error during login:", error);
+    return NextResponse.json({ error: "Login failed" }, { status: 400 });
+  }
 }
 
 export async function POST(request: NextRequest) {
   await connectDb();
-  const data = await request.json();
+  try {
+    const data = await request.json();
 
-  const companies = await Empresa.create({
-    ...data,
-    _id: new mongoose.Types.ObjectId().toString(),
-  });
+    const companies = await Empresa.create({
+      ...data,
+      _id: new mongoose.Types.ObjectId().toString(),
+    });
 
-  return NextResponse.json(companies);
+    return NextResponse.json(companies);
+  } catch (error) {
+    console.error("Error during login:", error);
+    return NextResponse.json({ error: "Login failed" }, { status: 400 });
+  }
 }
