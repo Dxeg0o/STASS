5b1d824baa2300531c42efa3850d5007965b7c2f
Cseguuu <cristobalsegu@gmail.com>
2024-12-30 15:48:54 -0300

    analisis

--- my-project/src/app/app/analisis/page.tsx
+++ my-project/src/app/app/analisis/page.tsx
@@ -1,14 +1,45 @@
-"use client";
-import { useState } from "react";
+'use client'
+import { useContext, useState } from "react";
 import QualityControlDashboard from "@/components/app/analisis/dashboard";
 import { ProductSelector } from "@/components/app/dashboard/selector-productos";
 import { Button } from "@/components/ui/button";
+import { AuthenticationContext } from "@/app/context/AuthContext";
 
 export default function QualityControlPage() {
   const [productSelected, setProductSelected] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<string | null>(null);
+  const { data, loading } = useContext(AuthenticationContext)
+  const handleStart = async () => {
+    if (!selectedProduct) {
+      alert("Por favor, selecciona un producto antes de comenzar.");
+      return;
+    }
 
-  const handleStart = () => {
-    setProductSelected(true);
+    try {
+      const response = await fetch("/api/analysis", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          empresa_id: data?.id_empresa, // Reemplazar con el ID de la empresa correcto
+          producto_id: selectedProduct,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Análisis creado:", data);
+        setProductSelected(true); // Cambiar la vista a QualityControlDashboard
+      } else {
+        const errorData = await response.json();
+        console.error("Error en la API:", errorData.message);
+        alert("Error al iniciar el análisis: " + errorData.message);
+      }
+    } catch (error) {
+      console.error("Error al llamar a la API:", error);
+      alert("Ocurrió un error inesperado. Inténtalo de nuevo.");
+    }
   };
 
   return (
@@ -20,7 +51,9 @@ export default function QualityControlPage() {
 
       {!productSelected ? (
         <div className="text-center">
-          <ProductSelector />
+          <ProductSelector
+            onProductSelect={(productId: string) => setSelectedProduct(productId)}
+          />
           <Button
             onClick={handleStart}
             className="mt-12 hover:bg-black/60 text-xl font-semibold p-6"
