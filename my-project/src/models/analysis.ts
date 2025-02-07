9f103ac23d01549a65d6a6bd6fd83154e6726577
Dxeg0o <dsoler.olguin@gmail.com>
2025-02-07 17:11:05 -0300

    update names

--- my-project/src/models/analysis.ts
+++ my-project/src/models/analysis.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const analisisSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  empresaId: { type: String, required: true, ref: "Empresa" },
-  fechaCreacion: { type: Date, required: true },
-  estado: {
-    type: String,
-    enum: ["completado", "en_progreso", "pendiente"],
-    required: true,
+const analisisSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    empresaId: { type: String, required: true, ref: "Empresa" },
+    fechaCreacion: { type: Date, required: true },
+    estado: {
+      type: String,
+      enum: ["completado", "en_progreso", "pendiente"],
+      required: true,
+    },
+    producto: { type: String, required: true }, // Array de IDs de productos analizados, podría ser solo un producto
   },
-  producto: { type: String, required: true }, // Array de IDs de productos analizados, podría ser solo un producto
-});
+  { collection: "Analisis" }
+);
 
 export default mongoose.models.Analisis ||
   mongoose.model("Analisis", analisisSchema);
--- my-project/src/models/predictions.ts
+++ my-project/src/models/predictions.ts
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 
-const prediccionSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  analisisId: { type: String, required: true, ref: "Analisis" },
-  atributos: {
-    tamaño: { type: String, required: true },
-    color: { type: String, required: true },
-  },
-  fecha: { type: Date, required: true },
-  resultado: { type: String, enum: ["apto", "no apto"], required: true },
-  subetiquetas: [
-    {
-      subetiquetaId: { type: String, required: true, ref: "Subetiqueta" },
+const prediccionSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    analisisId: { type: String, required: true, ref: "Analisis" },
+    atributos: {
+      tamaño: { type: String, required: true },
+      color: { type: String, required: true },
     },
-  ],
-});
+    fecha: { type: Date, required: true },
+    resultado: { type: String, enum: ["apto", "no apto"], required: true },
+    subetiquetas: [
+      {
+        subetiquetaId: { type: String, required: true, ref: "Subetiqueta" },
+      },
+    ],
+  },
+  { collection: "Predicciones" }
+);
 
 export default mongoose.models.Prediccion ||
   mongoose.model("Prediccion", prediccionSchema);
